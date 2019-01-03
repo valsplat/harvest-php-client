d44f47f65bb8f414f0d39f40100206760c53211b
Joris Leker <joris.leker@valsplat.nl>
2019-01-03 11:25:20 +0100

    #1 allow passing id to delete(), so it can be used without having to do a get() first

--- src/Valsplat/Harvest/Actions/Deletable.php
+++ src/Valsplat/Harvest/Actions/Deletable.php
@@ -7,12 +7,15 @@ trait Deletable
 
     /**
      * Delete an item
-     *
+     * @param $id (default to $this->id if not given)
      * @return mixed
      * @author Bjorn
      */
-    public function delete()
+    public function delete($id = null)
     {
+        if ($id !== null) {
+            $this->id = $id;
+        }
         return $this->connection()->delete($this->getEndpoint() . urlencode($this->id));
     }
 }
